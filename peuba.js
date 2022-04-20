729ae646efa5ebdc28966f015b573f750658b88a
Carlos Bouzas <carlosbouzas1991@gmail.com>
2022-04-21 00:16:20 +0200

    test rutas

--- peuba.js
+++ peuba.js
@@ -1,9 +1,34 @@
-    
-    const bcryptjs = require("bcryptjs");
+/*  const bcryptjs = require("bcryptjs");
     const salt = bcryptjs.genSaltSync();
     contraseñaA = bcryptjs.hashSync("admin", salt);
     contraseñaU = bcryptjs.hashSync("usuario", salt);
     contraseñaV = bcryptjs.hashSync("ventass", salt);
     console.log(contraseñaA);
     console.log(contraseñaU);
-    console.log(contraseñaV);
+    console.log(contraseñaV); */
+
+const rutasTotal = [
+  {path: "/user/user", methods: ["GET", "POST", "DELETE"]},
+  {path: "/user/activo", methods: ["GET", "PUT"]},
+];
+const rutasExaminar = [{path: "/user/user", methods: ["GET", "DELETE"]}];
+
+const pruebas = () => {};
+
+rutasTotal.map((ruta) => {
+  let coincidencia = rutasExaminar.filter((re) => re.path === ruta.path);
+  if (coincidencia.length > 0) {
+    ruta.methods.forEach((method) => {
+      if (coincidencia[0].methods.includes(method)) {
+        console.log("NO 401 - ", ruta.path, method);
+      } else {
+        console.log("401 - ", ruta.path, method);
+      }
+    });
+    // let coincidenciaMethods = coincidencia[0].methods.filter((cm) => cd);
+  } else {
+    ruta.methods.map((method) => {
+      console.log("401 - ", ruta.path, method);
+    });
+  }
+});
--- test/test/backend/user.test.js
+++ test/test/backend/user.test.js
@@ -1,59 +1,133 @@
 const request = require("supertest");
-const { expect } = require("chai");
+const {expect} = require("chai");
 
 const app = require("../../../server");
 
+const rutasTotal = [
+  {path: "/proyecto/user", methods: ["GET", "POST", "DELETE"]},
+  {path: "/user/activo", methods: ["GET", "PUT"]},
+];
+const rutasExaminar = [{path: "/proyecto/user", methods: ["GET", "DELETE"]}];
+
+const pruebas = (endpoint, method, expected, origin) => {
+  it(`${endpoint} - ${method} - ${origin} - ${expected}`, (done) => {
+    switch (method) {
+      case "GET":
+        request(app)
+          .get(endpoint)
+          .set("Accept", "application/json")
+          .set("origin", origin)
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            console.log(expected);
+            console.log(res);
+            expect(res.status).to.equal(expected);
+            done();
+          });
+        console.log("GET", endpoint, method, expected, origin);
+        break;
+      case "POST":
+        request(app)
+          .post(endpoint)
+          .set("Accept", "application/json")
+          .set("origin", origin)
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            console.log(expected);
+
+            expect(res.status).to.equal(expected);
+            done();
+          });
+        console.log("POST", endpoint, method, expected, origin);
+        break;
+      case "PUT":
+        request(app)
+          .put(endpoint)
+          .set("Accept", "application/json")
+          .set("origin", origin)
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            console.log(expected);
+
+            expect(res.status).to.equal(expected);
+            done();
+          });
+        console.log("PUT", endpoint, method, expected, origin);
+        break;
+      case "DELETE":
+        request(app)
+          .delete(endpoint)
+          .set("Accept", "application/json")
+          .set("origin", origin)
+          .expect("Content-Type", /json/)
+          .end((err, res) => {
+            console.log(expected);
+            expect(res.status).to.equal(expected);
+            done();
+          });
+        console.log("DELETE", endpoint, method, expected, origin);
+        break;
+    }
+  });
+};
+rutasTotal.map((ruta) => {
+  let coincidencia = rutasExaminar.filter((re) => re.path === ruta.path);
+  if (coincidencia.length > 0) {
+    ruta.methods.forEach((method) => {
+      if (coincidencia[0].methods.includes(method)) {
+        pruebas(ruta.path, method, !401, "LocalHost");
+      } else {
+        pruebas(ruta.path, method, 401, "LocalHost");
+      }
+    });
+  } else {
+    ruta.methods.map((method) => {
+      pruebas(ruta.path, method, 401, "LocalHost");
+    });
+  }
+});
 describe("Test Users", () => {
-  describe("GET /proyecto/user/get - Show Users for the aplicaction", () => {
-    it("T_USER001", (done) => {
-      request(app)
-        .get("/proyecto/user/get")
-        .set("Accept", "application/json")
-        .set("origin", "LocalHost")
-        .expect("Content-Type", /json/)
-        .end((err, res) => {
-          expect(200);
-          expect(res.body.users[0]).to.include(
-            {
-              nombre: "Usuario Administrador",
-              correo: "admin@correo.com",
-              role: "ADMIN_ROLE",
-              estado: true,
-              google: false
-            }
-        );
-        expect(res.body.users[1]).to.include(
-            {
-              nombre: "Usuario normal 1",
-              correo: "usuario1@correo.com",
-              role: "USER_ROLE",
-              estado: true,
-              google: false
-            }
-        );
-        expect(res.body.users[2]).to.include(
-            {
-              nombre: "Usuario ventas",
-              correo: "ventas@correo.com",
-              role: "VENTAS_ROLE",
-              estado: true,
-              google: false
-            }
-        );
+  xit("T_USER001", (done) => {
+    request(app)
+      .get("/proyecto/user/get")
+      .set("Accept", "application/json")
+      .set("origin", "LocalHost")
+      .expect("Content-Type", /json/)
+      .end((err, res) => {
+        expect(200);
+        expect(res.body.users[0]).to.include({
+          nombre: "Usuario Administrador",
+          correo: "admin@correo.com",
+          role: "ADMIN_ROLE",
+          estado: true,
+          google: false,
+        });
+        expect(res.body.users[1]).to.include({
+          nombre: "Usuario normal 1",
+          correo: "usuario1@correo.com",
+          role: "USER_ROLE",
+          estado: true,
+          google: false,
+        });
+        expect(res.body.users[2]).to.include({
+          nombre: "Usuario ventas",
+          correo: "ventas@correo.com",
+          role: "VENTAS_ROLE",
+          estado: true,
+          google: false,
+        });
         expect(res.body.users[0], res.body.users[1], res.body.users[2]).to.have.property("uid");
 
-        
         //expect(res.body.users[]);
         //expect(res.body.users[]);
         //expect(res.body.users[]);
-          done();
-          //    expect().
-          //expect().body
-        });
-    });
+        done();
+        //    expect().
+        //expect().body
+      });
   });
 
-  describe("POST /proyecto/user/post - Add new uer ", () => {
+  xdescribe("POST /proyecto/user/post - Add new uer ", () => {
     it("T_USER002 - Add user with incorrect email", (done) => {
       request(app)
         .post("/proyecto/user/post")
@@ -67,11 +141,11 @@ describe("Test Users", () => {
         .set("origin", "LocalHost")
         .expect("Content-Type", /json/)
         .end((err, res) => {
-         expect(422);
-         expect(res.body.msg).to.equal("Email no valido");
-          
+          expect(422);
+          expect(res.body.msg).to.equal("Email no valido");
+
           done();
-        })
+        });
     });
     it("T_USER003 - Add user without data", (done) => {
       request(app)
@@ -83,10 +157,10 @@ describe("Test Users", () => {
           expect(422);
           expect(res.body.msg).to.equal("No se introdujo ningun dato");
           done();
-        })
+        });
     });
 
-it("T_USER004 - Add user", (done) => {
+    xit("T_USER004 - Add user", (done) => {
       request(app)
         .post("/proyecto/user/post")
         .send({
@@ -107,13 +181,7 @@ it("T_USER004 - Add user", (done) => {
             role: "USER_ROLE",
           });
           done();
-        })
+        });
+    });
   });
-
-  });
-  
- 
-
-
-
 });
